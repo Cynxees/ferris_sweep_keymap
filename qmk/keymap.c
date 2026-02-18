b323d1b110d8037b590c6994434f109c6698ee08
Derwin Oesmanto <oesmantoderwin@gmail.com>
2026-02-18 11:14:07 +0700

    dvorak

--- qmk/keymap.c
+++ qmk/keymap.c
@@ -37,15 +37,15 @@ combo_t key_combos[COMBO_COUNT] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /*
 
- | '  | ,  | .  | P | Y |       | F | G | C | R | L |
+ | ;  | ,  | .  | P | Y |       | F | G | C | R | L |
  | A  | O  | E  | U | I |       | D | H | T | N | S |
- | ;  | Q  | J  | K | X |       | B | M | W | V | Z |
+ | '  | Q  | J  | K | X |       | B | M | W | V | Z |
            | FUN | SHIFT | | SPACE | SYMBOL |
   */
   [_DVORAK] = LAYOUT( /* Programmer Dvorak */
-    KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,            KC_F,    KC_G,  KC_C,    KC_R,   KC_L,
+    KC_SCLN, KC_COMM, KC_DOT,  KC_P,    KC_Y,            KC_F,    KC_G,  KC_C,    KC_R,   KC_L,
     KC_A,    KC_O,    KC_E,    KC_U,    KC_I,            KC_D,    KC_H,  KC_T,    KC_N,   KC_S,
-    KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,            KC_B,    KC_M,  KC_W,    KC_V,   KC_Z,
+    KC_QUOT, KC_Q,    KC_J,    KC_K,    KC_X,            KC_B,    KC_M,  KC_W,    KC_V,   KC_Z,
                                   MO(_FUNC_1), KC_LSFT, KC_SPC, MO(_SYMBOL_1)
   ),
 
